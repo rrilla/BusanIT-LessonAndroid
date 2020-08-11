f412e77f906afb0a139a9c8e1ff43ade073ec0ed
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-11 15:49:56 +0900

    End class6
    *뷰 컨테이너
    -뷰플리퍼

--- Ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ Ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnStop;
+    ViewFlipper vf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vf = findViewById(R.id.viewFlipper1);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vf.setFlipInterval(1000);
+                vf.startFlipping();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vf.stopFlipping();
+            }
+        });
     }
 }
