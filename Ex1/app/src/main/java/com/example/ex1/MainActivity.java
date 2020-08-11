eb7d7ae4b0e9ca66a39a6233c9a53634ef46c48b
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-11 12:55:03 +0900

    End class4
    고급위젯 활용 실습예제

--- Ex1/app/src/main/java/com/example/ex1/MainActivity.java
+++ Ex1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.wisettestapp2;
+package com.example.ex1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +10,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
     Chronometer chronometer;
-    Button btnStart, btnEnd;
+    RadioGroup rg;
+    Button btnEnd;
     RadioButton rbCal, rbTime;
-    CalendarView calendarView;
+    DatePicker datePicker;
     TimePicker timePicker;
     TextView tvResult;
     int selectYear, selectMonth, selectDay;
@@ -29,20 +32,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("시간 예약");
-        btnStart = findViewById(R.id.btnStart);
         btnEnd = findViewById(R.id.btnEnd);
         chronometer = findViewById(R.id.chronometer);
         rbCal = findViewById(R.id.rbCal);
         rbTime = findViewById(R.id.rbTime);
         timePicker = findViewById(R.id.timePick1);
-        calendarView = findViewById(R.id.calendarView1);
+        datePicker = findViewById(R.id.datePick1);
+        rg = findViewById(R.id.rg);
         tvResult = findViewById(R.id.tvResult);
         timePicker.setVisibility(View.INVISIBLE);
-        calendarView.setVisibility(View.INVISIBLE);
+        datePicker.setVisibility(View.INVISIBLE);
         rbTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calendarView.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
                 timePicker.setVisibility(View.VISIBLE);
             }
         });
@@ -51,18 +54,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 timePicker.setVisibility(View.INVISIBLE);
-                calendarView.setVisibility(View.VISIBLE);
+                datePicker.setVisibility(View.VISIBLE);
             }
         });
 
-        btnStart.setOnClickListener(new View.OnClickListener(){
+        chronometer.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 chronometer.setBase(SystemClock.elapsedRealtime());
                 chronometer.start();
                 chronometer.setTextColor(Color.RED);
+                rg.setVisibility(View.VISIBLE);
             }
         });
+
+       
 
         btnEnd.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -78,13 +84,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        /*calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 selectYear = year;
                 selectMonth = month+1;
                 selectDay = day;
             }
-        });
+        });*/
     }
 }
