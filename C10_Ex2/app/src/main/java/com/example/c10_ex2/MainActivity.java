7027dce71598aabf87e9c9dcb5720658e5f72f38
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-13 16:33:19 +0900

    End class7
    C10. 액티비티와 인텐트
    - 프로그램 완성,수정

--- C10_Ex2/app/src/main/java/com/example/c10_ex2/MainActivity.java
+++ C10_Ex2/app/src/main/java/com/example/c10_ex2/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.c10_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ImageView[] iv=new ImageView[9];
+    int[] ivIds={R.id.iv1,R.id.iv2,R.id.iv3,R.id.iv4,R.id.iv5,
+            R.id.iv6,R.id.iv7,R.id.iv8,R.id.iv9};
+    String[] titles={"독서하는 소녀","꽃장식 모자 소녀","부채를 든 소녀",
+            "이레느깡 단 베르양","잠자는 소녀","테라스의 두자매","피아노 레슨",
+            "피아노 앞의 소녀","해변에서"};
+    int[] voteCount=new int[9];
+    int[] drawbles={R.drawable.renoir01,R.drawable.renoir02,R.drawable.renoir03,
+            R.drawable.renoir04,R.drawable.renoir05,R.drawable.renoir06,
+            R.drawable.renoir07,R.drawable.renoir08,R.drawable.renoir09};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("명화 선호도 투표");
+        btn=findViewById(R.id.btnFinish);
+        for(int i=0; i<iv.length;i++){
+            final int idx=i;
+            iv[i]=findViewById(ivIds[i]);
+            iv[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    voteCount[idx]++;
+                    Toast.makeText(getApplicationContext(),
+                            titles[idx]+": 총"+voteCount[idx]+"표",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),
+                        SecondActivity.class);
+                int max=0;
+                int idx=-1;
+                for(int i=0; i<voteCount.length;i++){
+                    if(voteCount[i]>max){
+                        idx=i;
+                        max=voteCount[i];
+                    }
+                }
+                intent.putExtra("voteCount",voteCount);
+                intent.putExtra("titles",titles);
+                intent.putExtra("drawable",drawbles[idx]);
+                startActivity(intent);
+            }
+        });
     }
 }
--- C10_Ex2/app/src/main/java/com/example/c10_ex2/SecondActivity.java
+++ C10_Ex2/app/src/main/java/com/example/c10_ex2/SecondActivity.java
@@ -2,13 +2,48 @@ package com.example.c10_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView[] tv=new TextView[9];
+    RatingBar[] ratingBars=new RatingBar[9];
+    int[] tvIds={R.id.tv1,R.id.tv2,R.id.tv3,R.id.tv4,R.id.tv5,
+            R.id.tv6, R.id.tv7,R.id.tv8,R.id.tv9};
+    int[] ratIds={R.id.ratingBar1,R.id.ratingBar2,R.id.ratingBar3,
+            R.id.ratingBar4,R.id.ratingBar5,R.id.ratingBar6,
+            R.id.ratingBar7,R.id.ratingBar8,R.id.ratingBar9};
+    Button btn;
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        setTitle("투표결과");
+        Intent intent=getIntent();
+        int[] voteResult=intent.getIntArrayExtra("voteCount");
+        String[] titles=intent.getStringArrayExtra("titles");
+        int drawable=intent.getIntExtra("drawable",0);
+        iv=findViewById(R.id.imageView);
+        iv.setImageResource(drawable);
+
+        for(int i=0; i<tvIds.length;i++){
+            tv[i]=findViewById(tvIds[i]);
+            ratingBars[i]=findViewById(ratIds[i]);
+            tv[i].setText(titles[i]);
+            ratingBars[i].setRating((float)voteResult[i]);
+        }
+        btn=findViewById(R.id.btnBack);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
