927daa13dba3060f8360cf3ff7d9bd7b0d800c0b
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-17 12:11:55 +0900

    End class3
    프래그먼트
    
    -프래그먼트 활용 예제

---
+++ ExFragmentApp/app/src/main/java/com/example/exfragmentapp/Data.java
@@ -0,0 +1,9 @@
+package com.example.exfragmentapp;
+
+public class Data {
+    static String[] names = {"써니", "완득이", "괴물", "라디오 스타", "비열한 거리", "왕의 남자", "아일랜드",
+    "웰컴 투 동막골", "헬보이", "쥬라기 공원"};
+    static String[] img = {"@drawable/mov01", "@drawable/mov02", "@drawable/mov03", "@drawable/mov04", "@drawable/mov05",
+            "@drawable/mov06", "@drawable/mov07", "@drawable/mov08", "@drawable/mov09", "@drawable/mov12", };
+
+}
--- ExFragmentApp/app/src/main/java/com/example/exfragmentapp/MainActivity.java
+++ ExFragmentApp/app/src/main/java/com/example/exfragmentapp/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.exfragmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NameFragment.OnNameSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.name_container) != null){
+            if(savedInstanceState != null){
+                return;
+            }
+            NameFragment fragment = new NameFragment();
+            fragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.name_container, fragment).commit();
+
+        }
+    }
+
+    @Override
+    public void onNameSelected(int position) {
+        ImgFragment defFragment =
+                (ImgFragment)(getSupportFragmentManager().findFragmentById(R.id.img_container));
+        if(defFragment != null){
+            defFragment.updateDefinitionView(position);
+        }else{
+            ImgFragment newFragment = new ImgFragment();
+            Bundle args = new Bundle();
+            //args.putInt("position", position);
+            args.putInt(ImgFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.img_container, newFragment);
+//            ft.replace(R.id.definition_container, newFragment);
+//            ft.addToBackStack(null);
+            ft.commit();
+        }
     }
 }
