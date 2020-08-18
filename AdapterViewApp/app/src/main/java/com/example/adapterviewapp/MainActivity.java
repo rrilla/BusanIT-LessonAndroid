07686b6d601cf6196c7bb65c508cfa3ec724e556
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-18 10:09:56 +0900

    End class1
    C11. 어댑터뷰
    
    *복습
    -리스트뷰
    -그리드뷰

---
+++ AdapterViewApp/app/src/main/java/com/example/adapterviewapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.adapterviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*ListView listView;
+    String[] names = {"홍길동", "한재현", "김림용", "이하윤", "조양현"};
+    ArrayAdapter<String> adapter;*/
+    GridView gridView;
+    MyGridAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_layout);
+        gridView = findViewById(R.id.gridView1);
+        adapter = new MyGridAdapter(this);
+        gridView.setAdapter(adapter);
+
+
+
+
+        /*listView = findViewById(R.id.listView1);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            //(어댑터뷰, 뷰, 아이템의 위치, id )
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, names[position], Toast.LENGTH_LONG).show();
+            }
+        });*/
+    }
+
+    public class MyGridAdapter extends BaseAdapter{
+        Context context;
+        String[] title = {"써니", "완득이", "괴물", "라디오스타", "비열한거리", }
+        int[] ids = {R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10};
+
+        public MyGridAdapter(Context context){
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return ids.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ImageView iv = new ImageView(context);
+            iv.setLayoutParams(new GridView.LayoutParams(200, 300));
+            iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            iv.setPadding(5,5,5,5);
+            iv.setImageResource(ids[position]);
+            return iv;
+
+        }
+
+    }
+}
