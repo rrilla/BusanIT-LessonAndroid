1af2fd703a157d6cd65c40cb6b7b889f782746ac
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-20 15:46:52 +0900

    End class6
    스레드
    
    -애니메이션

---
+++ AnimationApp/app/src/main/java/com/example/animationapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.animationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        Animation animation= AnimationUtils.loadAnimation(
+                getApplicationContext(),
+                R.anim.scale);
+        btn.startAnimation(animation);
+    }
+}
