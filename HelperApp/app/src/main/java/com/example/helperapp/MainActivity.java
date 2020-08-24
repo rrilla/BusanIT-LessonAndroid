0f0f96c569c32a0493b0d46c9957afc324828595
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-08-24 12:53:35 +0900

    End class4
    -movieEx app 시작

--- HelperApp/app/src/main/java/com/example/helperapp/MainActivity.java
+++ HelperApp/app/src/main/java/com/example/helperapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helperapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -10,24 +11,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edDB,edTable;
+    EditText edName,edAge,edMobile;
     TextView tvResult;
-    Button btnDB,btnTable,btnInsert,btnSelect, btnUpdate,btnDelete;
+    Button btnDB,btnInsert,btnSelect, btnUpdate,btnDelete;
 
     DatabaseHelper helper;
     SQLiteDatabase database;
-    String tableName;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edDB=findViewById(R.id.editDB);
-        edTable=findViewById(R.id.editTable);
+
         tvResult=findViewById(R.id.tvResult);
         btnDB=findViewById(R.id.btnDB);
-        btnTable=findViewById(R.id.btnTable);
+        edName = findViewById(R.id.editName);
+        edAge = findViewById(R.id.editAge);
+        edMobile = findViewById(R.id.editMobile);
         btnInsert=findViewById(R.id.btnInsert);
         btnSelect=findViewById(R.id.btnselect);
         btnUpdate=findViewById(R.id.btnUpdate);
@@ -38,10 +38,75 @@ public class MainActivity extends AppCompatActivity {
                 createDatabase();
             }
         });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData();
+            }
+        });
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectData();
+            }
+        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateData();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteData();
+            }
+        });
     }
-    private void createDatabase(String name){
-        println("createDatebase() 호출함");
-        helper=new DatabaseHelper(this);
+
+    private void insertData() {
+        println("insertData() 호출함");
+        String name = edName.getText().toString();
+        int age = Integer.parseInt(edAge.getText().toString());
+        String mobile = edMobile.getText().toString();
+        String sql = "insert into emp(name, age, mobile) values('"+name+"','"+age+"','"+mobile+"')";
+        database.execSQL(sql);
+    }
+
+    private void selectData(){
+        println("selectData() 호출함");
+        String sql = "select * from emp";
+        Cursor cursor = database.rawQuery(sql,null);
+        int count = cursor.getCount();
+        println("레코드 수 : " + count );
+        for(int i=0; i<count; i++){
+            cursor.moveToNext();
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            int age = cursor.getInt(2);
+            String mobile = cursor.getString(3);
+            println("레코드 #" + i + " : " + id + ", " + name + ", " + age + ", " + mobile);
+        }
+        cursor.close();
+    }
+
+    private void updateData(){
+        String name = edName.getText().toString();
+        String mobile = edMobile.getText().toString();
+        String sql = "update emp set mobile='"+mobile+"' where name='"+name+"'";
+    }
+
+    private void deleteData(){
+        String name = edName.getText().toString();
+        String sql = "delete from emp where name='"+name+"'";
+        database.execSQL(sql);
+    }
+
+    private void createDatabase(){
+        println("createDatabase() 호출함");
+        helper = new DatabaseHelper(this);
+        database = helper.getWritableDatabase();
+        println("db 생성");
     }
 
     private void println(String data){
---
+++ MovieExApp/app/src/main/java/com/example/movieexapp/Movie.java
@@ -0,0 +1,4 @@
+package com.example.movieexapp;
+
+public class Movie {
+}
